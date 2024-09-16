0a92c6d916fe80d72420263d4ee0e16c15f60dcf
DD34THH <anshal.u.2002@gmail.com>
2024-09-17 00:07:37 +0530

    commit

--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../styles/Navbar.css'
+import logo from '../assets/logo-no-background.png'
 
 const Navbar = () => {
     return (
     <nav className="navbar">
-        <img src="logo-no-background.png" className="logo" />
+        <img src={logo} className="logo" />
         <div className="center-links">
             <a href="#">Home</a>
             <a href="#">Budget</a>
             <a href="#">History</a>
-            <a href="#">Suggestionss</a>
+            <a href="#">Suggestions</a>
         </div>
 
 
